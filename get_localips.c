b4320fe708ba58150f4d02c12ae706cbb1d7d0e0
lxy-mystery <42070586+lxy-mystery@users.noreply.github.com>
2023-07-24 09:57:49 +0800

    get local ips by ioctl

--- get_localips.c
+++ get_localips.c
@@ -85,3 +85,67 @@ void get_ip_for_interface(char* interface_name)
   sa.addr = &ifr.ifr_addr;
   printf("Address for %s: %s", interface_name, inet_ntoa(sa.in->sin_addr));
 }
+#include <stdio.h>
+#include <net/if.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+ 
+int get_localip(const char * eth_name, char *local_ip_addr)
+{
+	int ret = -1;
+    register int fd, intrface;
+    struct ifreq ifr[32];
+    struct ifconf ifc;
+ 
+    if (local_ip_addr == NULL || eth_name == NULL)
+	{
+        return ret;
+	}
+	if ((fd=socket(AF_INET, SOCK_DGRAM, 0)) > 0)
+	{
+		ifc.ifc_len = sizeof ifr;
+		ifc.ifc_buf = (caddr_t)ifr;
+		if (!ioctl(fd, SIOCGIFCONF, (char*)&ifc)) //获取所有接口信息
+		{
+			intrface = ifc.ifc_len / sizeof(struct ifreq);
+			while (intrface-- > 0)
+			{
+				//Get IP Address
+				if (!(ioctl(fd, SIOCGIFADDR, (char*)&ifr[intrface])))
+				{
+					if(strcmp(eth_name, ifr[intrface].ifr_name) == 0)
+					{
+						ret = 0;
+						sprintf(local_ip_addr, "%s", inet_ntoa(((struct sockaddr_in*)(&ifr[intrface].ifr_addr))->sin_addr));
+						break;
+					}
+				}
+			}
+		}
+	}
+	if (fd > 0)
+	{
+		close(fd);
+	}
+    return ret;
+}
+ 
+int main(int argc, const char **argv)
+{
+	int ret;
+	char local_ip[20] = {0};
+ 
+	ret = get_localip("eth0", local_ip);
+	if (ret == 0)
+	{
+		printf("local ip:%s\n", local_ip);
+	}
+	else
+	{
+		printf("get local ip failure\n");
+	}
+	return 0;
+}
